d0cf71d46ac920b23a216dba69950b726345eb16
Anthony Molina <antonio-molina@att.net>
2019-05-27 04:29:33 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -6,7 +6,7 @@
      index = "false" 
    elsif (board[index] == "")
      index = "false"
-   elsif (board[index["X", "O"] == nil])
+   elsif (board[index #{"X", "O"} == nil])
      index = "true"
    end 
  end
