3ad6c952cc78d590905b0a045d44c8d5e8efec24
Anthony Molina <antonio-molina@att.net>
2019-05-27 03:25:18 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,10 @@
 # code your #position_taken? method here!
+#position_taken?
+
+ def position_taken?(board, index) 
+   if board == ["X"]
+    return "false"
+  elsif board == ["O"]
+    return "false"
+  end
+end
