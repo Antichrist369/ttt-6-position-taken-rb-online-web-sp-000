f8427cc087bff6c6ad732f93a744fe19cce6b2b0
Anthony Molina <antonio-molina@att.net>
2019-05-27 03:52:06 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -8,3 +8,13 @@
      return = "true" 
    end 
  end
+ 
+ puts position_taken?(ttt_board.0)
+ 
+ puts position_taken?(ttt_board.1)
+ 
+ puts position_taken?(ttt_board.2)
+ 
+ puts position_taken?(ttt_board.3)
+ 
+ puts position_taken?(ttt_board.4)
