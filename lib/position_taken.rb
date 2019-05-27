b8b355b7a1db0dd53ecc559b65ac673a210f2b71
Anthony Molina <antonio-molina@att.net>
2019-05-27 03:35:11 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,5 +3,9 @@
 
  def position_taken?(board, index) 
    # taken = nil 
-   # if (board[index]) == " " || board[index] == || board[index] == nil
-   
+   # if (board[index] == " " || board[index] == || board[index] == nil)
+   #   taken = false 
+   # else 
+       taken = true 
+   # end 
+   # taken 
